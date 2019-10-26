f861cb2e62eebaf26fbba5ceaba830d9bfd8fecc
Chengz2019 <56699651+Chengz2019@users.noreply.github.com>
2019-10-25 23:13:57 -0400

    Update Main.java

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Finally I got this, I got pushed successfully! Yeah!");
+        System.out.println("I am wondering what will happen if I've done this operation.");
     }
 }
